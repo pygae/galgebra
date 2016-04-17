7803817ca9d49f6154d1763dccb5bcd796e0fecc
Sylvain Meunier <sylvain.meunier@gmail.com>
2016-04-17 21:51:48 +0200

    leo dorst book chapter 3 exercices (in progress)

--- tests/test_chapter3.py
+++ tests/test_chapter3.py
@@ -1,7 +1,7 @@
 import unittest
 
 from itertools import product
-from sympy import simplify, Symbol
+from sympy import simplify, Symbol, cos, sin, sqrt
 from ga import Ga
 from mv import Mv
 
@@ -184,7 +184,9 @@ class TestChapter3(unittest.TestCase):
     
     def test_3_10_1_1(self):
         """
-        Drills.
+        Let a = e_1 + e_2 and b = e_2 + e_3 in  a 3D Euclidean space R(3,0) with an orthonormal basis {e_1, e_2, e_3}.
+        Compute the following expressions, giving the results relative to the basis
+        {1, e_1, e_2, e_3, e_1 ^ e_2, e_2 ^ e_3, e_3 ^ e_1, e_1 ^ e_2 ^ e_3}.
         """
         
         Ga.dual_mode("Iinv+")
@@ -230,12 +232,12 @@ class TestChapter3(unittest.TestCase):
         self.assertEquals(e_1 < e_3, 0)
         self.assertEquals(e_2 < e_3, 0)
         self.assertEquals(((e_1 ^ e_2) ^ ((e_1 < e_3) + (e_2 < e_3))), 0)
-        self.assertEquals(a < (e_1 ^ e_2 ^ e_3), (e_2 ^ e_3) - (e_1 ^ e_3))
+        self.assertEquals(a < (e_1 ^ e_2 ^ e_3), (e_2 ^ e_3) + (e_3 ^ e_1))
         
         # f
         self.assertEquals(a < R.I_inv(), a < (-e_1 ^ e_2 ^ e_3))        # equals because we fixed the metric
         self.assertEquals(a < R.I_inv(), a < ((e_1 ^ e_2) ^ (-e_3)))    # almost last drill
-        self.assertEquals(a < R.I_inv(), -(e_2 ^ e_3) + (e_1 ^ e_3))
+        self.assertEquals(a < R.I_inv(), -(e_2 ^ e_3) - (e_3 ^ e_1))
         
         # g
         self.assertEquals((a ^ b) < R.I_inv(), -((b ^ a) < R.I_inv()))
@@ -252,6 +254,80 @@ class TestChapter3(unittest.TestCase):
         # h
         self.assertEquals(a < (b < R.I_inv()), (a ^ b) < R.I_inv())
         self.assertEquals(a < (b < R.I_inv()), e_3 - e_2 + e_1)
+    
+    
+    def test_3_10_1_2(self):
+        """
+        Compute the cosine of the angle between the following subspaces given on an orthonormal basis of a Euclidean space.        
+        """
+                
+        Ga.dual_mode("Iinv+")
+        
+        _R, e_1, e_2, e_3, e_4 = Ga.build('e*1|2|3|4', g='1 0 0 0, 0 1 0 0, 0 0 1 0, 0 0 0 1')                
+        alpha = Symbol('alpha', real=True)
+        theta = Symbol('theta', real=True)
+        
+        # e_1 and alpha * e_1                
+        A = e_1
+        B = alpha * e_1
+        cosine = (A | B.rev()) / (A.norm() * B.norm())        
+        self.assertEquals(A | B.rev(), e_1 | (alpha * e_1))
+        self.assertEquals(A.norm() * B.norm(), alpha)        
+        self.assertEquals(cosine, (e_1 | (alpha * e_1)) / alpha)
+        self.assertEquals(cosine, (alpha * (e_1 | e_1)) / alpha)
+        self.assertEquals(cosine, 1)
+        
+        # (e_1 + e_2) ^ e_3 and e_1 ^ e_3        
+        A = (e_1 + e_2) ^ e_3
+        B = e_1 ^ e_3
+        num = A | B.rev()
+        den = A.norm() * B.norm()
+        cosine = num / den          
+        self.assertEquals(num, ((e_1 ^ e_3) + (e_2 ^ e_3)) | (e_3 ^ e_1))
+        self.assertEquals(num, ((e_1 ^ e_3) | (e_3 ^ e_1)) + ((e_2 ^ e_3) | (e_3 ^ e_1)))
+        self.assertEquals(((e_1 ^ e_3) | (e_3 ^ e_1)), 1)
+        self.assertEquals(((e_2 ^ e_3) | (e_3 ^ e_1)), 0)
+        self.assertEquals(num, 1)        
+        self.assertEquals(den, ((e_1 + e_2) ^ e_3).norm() * (e_1 ^ e_3).norm())        
+        self.assertEquals(den, ((e_1 ^ e_3) + (e_2 ^ e_3)).norm())        
+        den2 = ((e_1 ^ e_3) + (e_2 ^ e_3)).norm2()        
+        self.assertEquals(den2, ((e_1 ^ e_3) + (e_2 ^ e_3)) | ((e_1 ^ e_3) + (e_2 ^ e_3)).rev())        
+        self.assertEquals(den2, ((e_1 ^ e_3) + (e_2 ^ e_3)) | ((e_3 ^ e_1) + (e_3 ^ e_2)))        
+        self.assertEquals(den2, ((e_1 ^ e_3) | (e_3 ^ e_1)) + ((e_1 ^ e_3) | (e_3 ^ e_2)) + ((e_2 ^ e_3) | (e_3 ^ e_1)) + ((e_2 ^ e_3) | (e_3 ^ e_2)))
+        self.assertEquals(den2, 1 + 0 + 0 + 1)        
+        self.assertEquals(cosine, 1 / sqrt(2))
+        
+        # (cos(theta) * e_1 + sin(theta) * e_2) ^ e_3 and e_2 ^ e_3        
+        A = (cos(theta) * e_1 + sin(theta) * e_2) ^ e_3
+        B = e_2 ^ e_3
+        num = A | B.rev()
+        den = A.norm() * B.norm()
+        cosine = num / den        
+        self.assertEquals(num, (cos(theta) * (e_1 ^ e_3) + sin(theta) * (e_2 ^ e_3)) | (e_3 ^ e_2))
+        self.assertEquals(num, ((cos(theta) * (e_1 ^ e_3)) | (e_3 ^ e_2)) + ((sin(theta) * (e_2 ^ e_3)) | (e_3 ^ e_2)))
+        self.assertEquals((cos(theta) * (e_1 ^ e_3)) | (e_3 ^ e_2), 0)
+        self.assertEquals((sin(theta) * (e_2 ^ e_3)) | (e_3 ^ e_2), sin(theta))
+        self.assertEquals(num, sin(theta))
+        self.assertEquals(den, ((cos(theta) * e_1 + sin(theta) * e_2) ^ e_3).norm() * (e_2 ^ e_3).norm())
+        self.assertEquals(den, ((cos(theta) * e_1 + sin(theta) * e_2) ^ e_3).norm())        
+        den2 = ((cos(theta) * e_1 + sin(theta) * e_2) ^ e_3).norm2()
+        self.assertEquals(den2, ((cos(theta) * e_1 + sin(theta) * e_2) ^ e_3) | ((cos(theta) * e_1 + sin(theta) * e_2) ^ e_3).rev())
+        self.assertEquals(den2, (cos(theta) * (e_1 ^ e_3) + sin(theta) * (e_2 ^ e_3)) | (cos(theta) * (e_1 ^ e_3) + sin(theta) * (e_2 ^ e_3)).rev())
+        self.assertEquals(den2, (cos(theta) * (e_1 ^ e_3) + sin(theta) * (e_2 ^ e_3)) | (cos(theta) * (e_3 ^ e_1) + sin(theta) * (e_3 ^ e_2)))
+        self.assertEquals(den2, ((cos(theta) * (e_1 ^ e_3)) | (cos(theta) * (e_3 ^ e_1))) + ((cos(theta) * (e_1 ^ e_3)) | (sin(theta) * (e_3 ^ e_2))) + ((sin(theta) * (e_2 ^ e_3)) | (cos(theta) * (e_3 ^ e_1))) + ((sin(theta) * (e_2 ^ e_3)) | (sin(theta) * (e_3 ^ e_2))))
+        self.assertEquals(den2, cos(theta) * cos(theta) + sin(theta) * sin(theta))
+        self.assertEquals(den2, 1)        
+        self.assertEquals(cosine, sin(theta))
+                
+        # e_1 ^ e_2 and e_3 ^ e_4        
+        A = e_1 ^ e_2
+        B = e_3 ^ e_4
+        num = A | B.rev()
+        den = A.norm() * B.norm()
+        cosine = num / den
+        self.assertEquals(num, (e_1 ^ e_2) | (e_4 ^ e_3))
+        self.assertEquals(num, 0)        
+        self.assertEquals(cosine, 0)
 
 
 if __name__ == '__main__':
