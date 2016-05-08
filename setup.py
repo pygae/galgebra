7ec591e8138fb2a90a6d59008757385052fb4b0d
utensil <utensilcandel@gmail.com>
2016-05-09 00:37:01 +0800

    Add option --noconflict

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 from distutils.core import Extension
+import sys
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--noconflict', dest='noconflict', action='store_true', help='Prefix modules with namespace "galgebra"')
+parser.set_defaults(noconflict=False)
+(args, _) = parser.parse_known_args()
+noconflict = vars(args)['noconflict']
+# prevent setup() to see the option
+try:
+    sys.argv.remove('--noconflict')
+except Exception as e:
+    pass
+ga_namespace = 'galgebra' if noconflict else ''
 
 setup(name='galgebra',
       version='0.4.0',
@@ -11,7 +24,7 @@ setup(name='galgebra',
       license='BSD',
       py_modules=['ga','mv','lt','metric','printer'],
       #packages=find_packages(''),
-      package_dir={'':'galgebra'},
+      package_dir={ga_namespace:'galgebra'},
       install_requires = [
                 'sympy',
                 'numpy'],
