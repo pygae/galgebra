7abb1055bf4901ef68cd2b38f9c984c58efea9e9
utensil <utensilcandel@gmail.com>
2016-06-09 20:25:25 +0800

    Pass the rest of tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -18,16 +18,16 @@ def F(x):
     Fx = HALF*((x*x)*n+2*x-nbar)
     return(Fx)
 
-def make_vector(a,n = 3):
+def make_vector(a, n=3, ga=None):
     if isinstance(a,str):
-        sym_str = ''
+        v = zero
         for i in range(n):
-            sym_str += a+str(i+1)+' '
-        sym_lst = list(symbols(sym_str))
-        sym_lst.append(ZERO)
-        sym_lst.append(ZERO)
-        a = Mv(sym_lst,'vector')
-    return(F(a))
+            a_i = Symbol(a+str(i+1))
+            v += a_i*ga.basis[i]
+        v = ga.mv(v)
+        return(F(v))
+    else:
+        return(F(a))
 
 class TestTest(unittest.TestCase):
     def setUp(self):
@@ -294,151 +294,161 @@ class TestTest(unittest.TestCase):
         assert str(expand(simplify(expand(C)))) == '-(X.Y)/((X.e)*(Y.e)) + 1'
 
         return
-    #
-    # def test_conformal_representations_of_circles_lines_spheres_and_planes():
-    #     global n,nbar
-    #
-    #
-    #     metric = '1 0 0 0 0,0 1 0 0 0,0 0 1 0 0,0 0 0 0 2,0 0 0 2 0'
-    #
-    #     (e1,e2,e3,n,nbar) = Mv.setup('e_1 e_2 e_3 n nbar',metric)
-    #
-    #     e = n+nbar
-    #     #conformal representation of points
-    #
-    #     A = make_vector(e1)
-    #     B = make_vector(e2)
-    #     C = make_vector(-e1)
-    #     D = make_vector(e3)
-    #     X = make_vector('x',3)
-    #
-    #     assert str(A) == 'e_1 + 1/2*n - 1/2*nbar'
-    #     assert str(B) == 'e_2 + 1/2*n - 1/2*nbar'
-    #     assert str(C) == '-e_1 + 1/2*n - 1/2*nbar'
-    #     assert str(D) == 'e_3 + 1/2*n - 1/2*nbar'
-    #     assert str(X) == 'x1*e_1 + x2*e_2 + x3*e_3 + ((x1**2 + x2**2 + x3**2)/2)*n - 1/2*nbar'
-    #
-    #     assert str((A^B^C^X)) == '-x3*e_1^e_2^e_3^n + x3*e_1^e_2^e_3^nbar + ((x1**2 + x2**2 + x3**2 - 1)/2)*e_1^e_2^n^nbar'
-    #     assert str((A^B^n^X)) == '-x3*e_1^e_2^e_3^n + ((x1 + x2 - 1)/2)*e_1^e_2^n^nbar + x3/2*e_1^e_3^n^nbar - x3/2*e_2^e_3^n^nbar'
-    #     assert str((((A^B)^C)^D)^X) == '((-x1**2 - x2**2 - x3**2 + 1)/2)*e_1^e_2^e_3^n^nbar'
-    #     assert str((A^B^n^D^X)) == '((-x1 - x2 - x3 + 1)/2)*e_1^e_2^e_3^n^nbar'
-    #
-    #     L = (A^B^e)^X
-    #
-    #     assert str(L) == '-x3*e_1^e_2^e_3^n - x3*e_1^e_2^e_3^nbar + (-x1**2/2 + x1 - x2**2/2 + x2 - x3**2/2 - 1/2)*e_1^e_2^n^nbar + x3*e_1^e_3^n^nbar - x3*e_2^e_3^n^nbar'
 
-    #     return
-    #
-    # def test_properties_of_geometric_objects():
-    #
-    #     metric = '# # # 0 0,'+ \
-    #              '# # # 0 0,'+ \
-    #              '# # # 0 0,'+ \
-    #              '0 0 0 0 2,'+ \
-    #              '0 0 0 2 0'
-    #
-    #     (p1,p2,p3,n,nbar) = Mv.setup('p1 p2 p3 n nbar',metric)
-    #
-    #     P1 = F(p1)
-    #     P2 = F(p2)
-    #     P3 = F(p3)
-    #
-    #     L = P1^P2^n
-    #     delta = (L|n)|nbar
-    #     assert str(delta) == '2*p1 - 2*p2'
-    #
-    #     C = P1^P2^P3
-    #     delta = ((C^n)|n)|nbar
-    #     assert str(delta) == '2*p1^p2 - 2*p1^p3 + 2*p2^p3'
-    #     assert str((p2-p1)^(p3-p1)) == 'p1^p2 - p1^p3 + p2^p3'
+    def test_conformal_representations_of_circles_lines_spheres_and_planes(self):
+        global n,nbar
 
-    #     return
-    #
-    # def test_extracting_vectors_from_conformal_2_blade():
-    #
-    #     metric = ' 0 -1 #,'+ \
-    #              '-1  0 #,'+ \
-    #              ' #  # #,'
-    #
-    #     (P1,P2,a) = Mv.setup('P1 P2 a',metric)
-    #
-    #     B = P1^P2
-    #     Bsq = B*B
-    #     assert str(Bsq) == '1'
-    #     ap = a-(a^B)*B
-    #     assert str(ap) == '-(P2.a)*P1 - (P1.a)*P2'
-    #
-    #     Ap = ap+ap*B
-    #     Am = ap-ap*B
-    #
-    #     assert str(Ap) == '-2*(P2.a)*P1'
-    #     assert str(Am) == '-2*(P1.a)*P2'
-    #
-    #     assert str(Ap*Ap) == '0'
-    #     assert str(Am*Am) == '0'
-    #
-    #     aB = a|B
-    #     assert str(aB) == '-(P2.a)*P1 + (P1.a)*P2'
+        g = '1 0 0 0 0,0 1 0 0 0,0 0 1 0 0,0 0 0 0 2,0 0 0 2 0'
 
-    #     return
-    #
-    # def test_reciprocal_frame_test():
-    #
-    #     metric = '1 # #,'+ \
-    #              '# 1 #,'+ \
-    #              '# # 1,'
-    #
-    #     (e1,e2,e3) = Mv.setup('e1 e2 e3',metric)
-    #
-    #     E = e1^e2^e3
-    #     Esq = (E*E).scalar()
-    #     assert str(E) == 'e1^e2^e3'
-    #     assert str(Esq) == '(e1.e2)**2 - 2*(e1.e2)*(e1.e3)*(e2.e3) + (e1.e3)**2 + (e2.e3)**2 - 1'
-    #     Esq_inv = 1/Esq
-    #
-    #     E1 = (e2^e3)*E
-    #     E2 = (-1)*(e1^e3)*E
-    #     E3 = (e1^e2)*E
-    #
-    #     assert str(E1) == '((e2.e3)**2 - 1)*e1 + ((e1.e2) - (e1.e3)*(e2.e3))*e2 + (-(e1.e2)*(e2.e3) + (e1.e3))*e3'
-    #     assert str(E2) == '((e1.e2) - (e1.e3)*(e2.e3))*e1 + ((e1.e3)**2 - 1)*e2 + (-(e1.e2)*(e1.e3) + (e2.e3))*e3'
-    #     assert str(E3) == '(-(e1.e2)*(e2.e3) + (e1.e3))*e1 + (-(e1.e2)*(e1.e3) + (e2.e3))*e2 + ((e1.e2)**2 - 1)*e3'
-    #
-    #     w = (E1|e2)
-    #     w = w.expand()
-    #     assert str(w) == '0'
-    #
-    #     w = (E1|e3)
-    #     w = w.expand()
-    #     assert str(w) == '0'
-    #
-    #     w = (E2|e1)
-    #     w = w.expand()
-    #     assert str(w) == '0'
-    #
-    #     w = (E2|e3)
-    #     w = w.expand()
-    #     assert str(w) == '0'
-    #
-    #     w = (E3|e1)
-    #     w = w.expand()
-    #     assert str(w) == '0'
-    #
-    #     w = (E3|e2)
-    #     w = w.expand()
-    #     assert str(w) == '0'
-    #
-    #     w = (E1|e1)
-    #     w = (w.expand()).scalar()
-    #     Esq = expand(Esq)
-    #     assert str(simplify(w/Esq)) == '1'
-    #
-    #     w = (E2|e2)
-    #     w = (w.expand()).scalar()
-    #     assert str(simplify(w/Esq)) == '1'
-    #
-    #     w = (E3|e3)
-    #     w = (w.expand()).scalar()
-    #     assert str(simplify(w/Esq)) == '1'
+        cnfml3d = Ga('e_1 e_2 e_3 n nbar',g=g)
 
-    #     return
+        (e1,e2,e3,n,nbar) = cnfml3d.mv()
+
+        e = n+nbar
+
+        #conformal representation of points
+
+        A = make_vector(e1,ga=cnfml3d)    # point a = (1,0,0)  A = F(a)
+        B = make_vector(e2,ga=cnfml3d)    # point b = (0,1,0)  B = F(b)
+        C = make_vector(-e1,ga=cnfml3d)   # point c = (-1,0,0) C = F(c)
+        D = make_vector(e3,ga=cnfml3d)    # point d = (0,0,1)  D = F(d)
+        X = make_vector('x',3,ga=cnfml3d)
+
+        assert str(A) == 'e_1 + n/2 - nbar/2'
+        assert str(B) == 'e_2 + n/2 - nbar/2'
+        assert str(C) == '-e_1 + n/2 - nbar/2'
+        assert str(D) == 'e_3 + n/2 - nbar/2'
+        assert str(X) == 'x1*e_1 + x2*e_2 + x3*e_3 + (x1**2/2 + x2**2/2 + x3**2/2)*n - nbar/2'
+
+        assert str((A^B^C^X)) == '-x3*e_1^e_2^e_3^n + x3*e_1^e_2^e_3^nbar + (x1**2/2 + x2**2/2 + x3**2/2 - 1/2)*e_1^e_2^n^nbar'
+        assert str((A^B^n^X)) == '-x3*e_1^e_2^e_3^n + (x1/2 + x2/2 - 1/2)*e_1^e_2^n^nbar + x3*e_1^e_3^n^nbar/2 - x3*e_2^e_3^n^nbar/2'
+        assert str((((A^B)^C)^D)^X) == '(-x1**2/2 - x2**2/2 - x3**2/2 + 1/2)*e_1^e_2^e_3^n^nbar'
+        assert str((A^B^n^D^X)) == '(-x1/2 - x2/2 - x3/2 + 1/2)*e_1^e_2^e_3^n^nbar'
+
+        L = (A^B^e)^X
+
+        assert str(L) == '-x3*e_1^e_2^e_3^n - x3*e_1^e_2^e_3^nbar + (-x1**2/2 + x1 - x2**2/2 + x2 - x3**2/2 - 1/2)*e_1^e_2^n^nbar + x3*e_1^e_3^n^nbar - x3*e_2^e_3^n^nbar'
+
+        return
+
+    def test_properties_of_geometric_objects(self):
+
+        global n, nbar
+
+        g = '# # # 0 0,'+ \
+            '# # # 0 0,'+ \
+            '# # # 0 0,'+ \
+            '0 0 0 0 2,'+ \
+            '0 0 0 2 0'
+
+        c3d = Ga('p1 p2 p3 n nbar',g=g)
+
+        (p1,p2,p3,n,nbar) = c3d.mv()
+
+        P1 = F(p1)
+        P2 = F(p2)
+        P3 = F(p3)
+
+        L = P1^P2^n
+        delta = (L|n)|nbar
+        assert str(delta) == '2*p1 - 2*p2'
+
+        C = P1^P2^P3
+        delta = ((C^n)|n)|nbar
+        assert str(delta) == '2*p1^p2 - 2*p1^p3 + 2*p2^p3'
+        assert str((p2-p1)^(p3-p1)) == 'p1^p2 - p1^p3 + p2^p3'
+
+        return
+
+    def test_extracting_vectors_from_conformal_2_blade(self):
+
+        g = '0 -1 #,'+ \
+            '-1 0 #,'+ \
+            '# # #'
+
+        e2b = Ga('P1 P2 a',g=g)
+
+        (P1,P2,a) = e2b.mv()
+
+        B = P1^P2
+        Bsq = B*B
+        assert str(Bsq) == '1'
+        ap = a-(a^B)*B
+        assert str(ap) == '-(P2.a)*P1 - (P1.a)*P2'
+
+        Ap = ap+ap*B
+        Am = ap-ap*B
+
+        assert str(Ap) == '-2*(P2.a)*P1'
+        assert str(Am) == '-2*(P1.a)*P2'
+
+        assert str(Ap*Ap) == '0'
+        assert str(Am*Am) == '0'
+
+        aB = a|B
+        assert str(aB) == '-(P2.a)*P1 + (P1.a)*P2'
+
+        return
+
+    def test_reciprocal_frame_test(self):
+
+        g = '1 # #,'+ \
+            '# 1 #,'+ \
+            '# # 1'
+
+        g3dn = Ga('e1 e2 e3',g=g)
+
+        (e1,e2,e3) = g3dn.mv()
+
+        E = e1^e2^e3
+        Esq = (E*E).scalar()
+        assert str(E) == 'e1^e2^e3'
+        assert str(Esq) == '(e1.e2)**2 - 2*(e1.e2)*(e1.e3)*(e2.e3) + (e1.e3)**2 + (e2.e3)**2 - 1'
+        Esq_inv = 1/Esq
+
+        E1 = (e2^e3)*E
+        E2 = (-1)*(e1^e3)*E
+        E3 = (e1^e2)*E
+
+        assert str(E1) == '((e2.e3)**2 - 1)*e1 + ((e1.e2) - (e1.e3)*(e2.e3))*e2 + (-(e1.e2)*(e2.e3) + (e1.e3))*e3'
+        assert str(E2) == '((e1.e2) - (e1.e3)*(e2.e3))*e1 + ((e1.e3)**2 - 1)*e2 + (-(e1.e2)*(e1.e3) + (e2.e3))*e3'
+        assert str(E3) == '(-(e1.e2)*(e2.e3) + (e1.e3))*e1 + (-(e1.e2)*(e1.e3) + (e2.e3))*e2 + ((e1.e2)**2 - 1)*e3'
+
+        w = (E1|e2)
+        w = w.expand()
+        assert str(w) == '0'
+
+        w = (E1|e3)
+        w = w.expand()
+        assert str(w) == '0'
+
+        w = (E2|e1)
+        w = w.expand()
+        assert str(w) == '0'
+
+        w = (E2|e3)
+        w = w.expand()
+        assert str(w) == '0'
+
+        w = (E3|e1)
+        w = w.expand()
+        assert str(w) == '0'
+
+        w = (E3|e2)
+        w = w.expand()
+        assert str(w) == '0'
+
+        w = (E1|e1)
+        w = (w.expand()).scalar()
+        Esq = expand(Esq)
+        assert str(simplify(w/Esq)) == '1'
+
+        w = (E2|e2)
+        w = (w.expand()).scalar()
+        assert str(simplify(w/Esq)) == '1'
+
+        w = (E3|e3)
+        w = (w.expand()).scalar()
+        assert str(simplify(w/Esq)) == '1'
+
+        return
