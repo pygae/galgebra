3a69439762aaca2ea7935cbd5bba69a210319d3f
Hugo Hadfield <hadfield.hugo@gmail.com>
2017-12-07 15:22:34 +0000

    started prepping for pypi upload

--- setup.py
+++ setup.py
@@ -3,18 +3,24 @@ from setuptools import setup, find_packages
 from distutils.core import Extension
 
 
+VERSION = '0.4.0'
+LONG_DESCRIPTION = """
+A symbolic geometric algebra module for python. BSD License.
+"""
+
 setup(name='galgebra',
-      version='0.4.0',
+      version=VERSION,
       description='Symbolic Geometric Algebra/Calculus modules for sympy',
       author='Alan Bromborsky',
-      author_email='abrombo@verizon.net',
+      author_email='hadfield.hugo@gmail.com',
+      url='https://github.com/hugohadfield/galgebra',
       license='BSD',
       packages=find_packages(),
       package_dir={'galgebra':'galgebra'},
       install_requires = [
                 'sympy',
                 'numpy'],
-      #long_description=read('README'),
+      long_description=LONG_DESCRIPTION,
       classifiers=[
             'Development Status :: 4 - Beta',
             'Environment :: Console',
