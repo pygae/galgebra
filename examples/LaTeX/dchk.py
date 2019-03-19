34c0cea9333b1100b7ffc15ade2c01faeff50028
utensil <utensilcandel@gmail.com>
2019-03-19 19:58:01 +0800

    futurize -x libfuturize.fixes.fix_division_safe -w dchk.py

--- examples/LaTeX/dchk.py
+++ examples/LaTeX/dchk.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 from sympy import symbols, sin
-from printer import Format, xpdf, Fmt
-from ga import Ga
+from galgebra.printer import Format, xpdf, Fmt
+from galgebra.ga import Ga
 
 Format()
 
@@ -8,11 +9,13 @@ g = '# 0 #, 0 # 0, # 0 #'
 
 (g3d, ea, eab, eb) = Ga.build('e_a e_ab e_b', g=g)
 
-print g3d.g
+print(g3d.g)
 
 v = g3d.mv('v','vector')
 B = g3d.mv('B','bivector')
 
-print v
-print B
-xpdf()
+print(v)
+print(B)
+
+# xpdf()
+xpdf(pdfprog=None)
