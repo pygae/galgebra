0de7425615bc916d9ebc0ade46d10787b17a212c
Eric Wieser <wieser.eric@gmail.com>
2019-12-02 09:49:47 +0000

    Check `isinstance(f, bool)` just once

--- galgebra/mv.py
+++ galgebra/mv.py
@@ -181,17 +181,16 @@ class Mv(object):
         if utils.isstr(__name_or_coeffs):
             name = __name_or_coeffs
             f = kwargs['f']
-            if isinstance(f, bool) and not f:  #Is a constant mulitvector function
-                return sum([Symbol(add_superscript(name, super_script), real=True) * base
-                                for (super_script, base) in zip(ga.blade_super_scripts[grade], ga.blades[grade])])
-
-            else:
-                if isinstance(f, bool):  #Is a multivector function of all coordinates
+            if isinstance(f, bool):
+                if f:  # Is a multivector function of all coordinates
                     return sum([Function(add_superscript(name, super_script), real=True)(*ga.coords) * base
-                        for (super_script, base) in zip(ga.blade_super_scripts[grade], ga.blades[grade])])
-                else: #Is a multivector function of tuple f variables
-                    return sum([Function(add_superscript(name, super_script), real=True)(*f) * base
-                        for (super_script, base) in zip(ga.blade_super_scripts[grade], ga.blades[grade])])
+                                for (super_script, base) in zip(ga.blade_super_scripts[grade], ga.blades[grade])])
+                else:  # Is a constant multivector function
+                    return sum([Symbol(add_superscript(name, super_script), real=True) * base
+                                for (super_script, base) in zip(ga.blade_super_scripts[grade], ga.blades[grade])])
+            else:  # Is a multivector function of tuple f variables
+                return sum([Function(add_superscript(name, super_script), real=True)(*f) * base
+                            for (super_script, base) in zip(ga.blade_super_scripts[grade], ga.blades[grade])])
         elif isinstance(__name_or_coeffs, (list, tuple)):
             coeffs = __name_or_coeffs
             if len(coeffs) <= len(ga.blades[grade]):
