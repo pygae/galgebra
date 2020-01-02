5dce876311c4a287c76105f865654ab40527fbb8
Eric Wieser <wieser.eric@gmail.com>
2020-01-02 16:19:33 +0000

    Remove __future__ imports

--- doc/python/LinearTrans.py
+++ doc/python/LinearTrans.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division
-from __future__ import print_function
 from sympy import symbols, sin, cos
 from galgebra.ga import Ga
 from galgebra.printer import Format, xpdf
