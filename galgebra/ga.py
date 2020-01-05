80982623e61ac21ebe35a20c5d1c85a9e902951c
Eric Wieser <wieser.eric@gmail.com>
2020-01-05 21:11:59 +0000

    Merge `deprecated.ReciprocalFrame` with `ga.Ga.ReciprocalFrame`.
    The former differed only in the use of `S(1)` instead of `1`, which is better anyway.
    
    This moves the function from the former to the latter, then has the former call the latter.

--- galgebra/ga.py
+++ galgebra/ga.py
@@ -1867,7 +1867,7 @@ class Ga(metric.Metric):
         for igrade in index[-2:]:
             grade = []
             for iblade in igrade:
-                blade = self.mv(1, 'scalar')
+                blade = self.mv(S(1), 'scalar')
                 for ibasis in iblade:
                     blade ^= basis[ibasis]
                 blade = blade.trigsimp()
@@ -1879,7 +1879,7 @@ class Ga(metric.Metric):
         duals = copy.copy(MFbasis[-2])
 
         duals.reverse()
-        sgn = 1
+        sgn = S(1)
         rbasis = []
         for dual in duals:
             recpv = (sgn * dual * E).trigsimp()
