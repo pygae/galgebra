7d04a420c690484d63efebf91a14b68af5b887ff
Eric Wieser <wieser.eric@gmail.com>
2020-04-20 11:42:27 +0100

    Add CI for python 3.8
    Removes the stale `python-3.7-sympy-1.4` job which was identical to `python-3.7-sympy-1.3`.
    
    Note for this to pass the notebook tests we need to also fix warnings about misuse of the `is` operator, which this patch includes.

--- setup.py
+++ setup.py
@@ -32,5 +32,6 @@ setup(name='galgebra',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Scientific/Engineering :: Mathematics',
             'Topic :: Scientific/Engineering :: Physics'])
