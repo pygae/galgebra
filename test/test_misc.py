7bf8bbd71d98872d54d516cec49467529a39f175
Eric Wieser <wieser.eric@gmail.com>
2020-04-23 18:54:32 +0100

    Add a cached_property implentation
    We have a lot of attributes in galgebra that are cached in inconsistent ways.
    Future commits will use this unified way to spell them.
    
    We use `_utils` instead of `utils` so that this does not become public API.

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,4 +1,9 @@
+import sys
+
+import pytest
+
 from galgebra.ga import OrderedBiMap
+from galgebra._utils import cached_property
 
 
 class TestOrderedBiMap:
@@ -12,3 +17,40 @@ class TestOrderedBiMap:
         # check that we can use this like a generic type
         def foo() -> OrderedBiMap[str, int]:
             pass
+
+
+class TestCachedProperty:
+
+    def test_cache(self):
+
+        class X:
+            _val = 0
+            @cached_property
+            def val(self):
+                try:
+                    return self._val
+                finally:
+                    self._val += 1
+
+        # property is computed only when the cache is clear
+        x = X()
+        assert x.val == 0
+        assert x.val == 0
+        del x.val
+        assert x.val == 1
+        assert x.val == 1
+        del x.val
+        assert x.val == 2
+        assert x.val == 2
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 6),
+        reason='__set_name__ was not added until Python 3.6'
+    )
+    def test_annotation(self):
+        class X:
+            @cached_property
+            def val(self) -> 'int':
+                pass
+
+        assert X.__annotations__['val'] == 'int'
