9b660770927e55552b3adeb2ed12c1e472233a2d
Eric Wieser <wieser.eric@gmail.com>
2020-05-05 17:42:12 +0100

    Add a warning explaining that the api docs are duplicated [ci skip] (#302)

--- galgebra/__init__.py
+++ galgebra/__init__.py
@@ -1,4 +1,4 @@
-"""
+r"""
 .. currentmodule:: galgebra
 ========================================
 galgebra (:mod:`galgebra`)
@@ -19,6 +19,17 @@ Submodules
     dop
     printer
     utils
+
+.. note::
+
+    Some functions may be better documented by the original non-generated docs
+    on the :doc:`module-components` page, but there is a risk the descriptions
+    there are outdated.
+
+    If you would like to help with merging the descriptions here with a
+    description on that page, please head over to :issue:`300` on GitHub, where
+    there's an explanation of how to do so. Even merging just one function
+    explanation helps!
 """
 
 from ._version import __version__  # noqa: F401
