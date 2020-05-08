33829a039a7cac4fa002286c9c0b5ec1c123b433
Eric Wieser <wieser.eric@gmail.com>
2020-05-08 12:14:24 +0100

    Move method docstrings from rst to python for deprecated.MV
    These are taken almost verbatim, with some added line wrapping.
    Note that the footnote needs to move with them.

--- galgebra/deprecated.py
+++ galgebra/deprecated.py
@@ -18,7 +18,30 @@ class MV(Mv):
             return gstr
 
     @staticmethod
-    def setup(basis, metric=None, coords=None, rframe=False, debug=False, curv=(None, None)):
+    def setup(basis, metric=None, coords=None, rframe=False, debug=False, curv=(None, None)) -> list:
+        """
+        This function allows a single geometric algebra to be created.
+
+        If the function is called more than once the old geometric algebra is
+        overwritten by the new geometric algebra. The named input ``metric``
+        is the same as the named input ``g`` in the current version of
+        *galgebra*. Likewise, ``basis``, ``coords``, and ``debug`` are the same
+        in the old and current versions of *galgebra* [17]_.
+        Due to improvements in *sympy* the inputs ``rframe`` and ``curv[1]`` are
+        no longer required. ``curv[0]`` is the vector function (list or tuple of
+        scalar functions) of the coordinates required to define a vector manifold.
+        For compatibility with the old version of *galgebra* if ``curv`` is used
+        ``metric`` should be a orthonormal Euclidean metric of the same
+        dimension as ``curv[0]``.
+
+        It is strongly suggested that one use the new methods of defining a
+        geometric algebra on a manifold.
+
+        .. [17]
+            If the metric is input as a list or list or lists the object is no
+            longer quoted (input as a string). For example the old
+            ``metric='[1,1,1]'`` becomes ``metric=[1,1,1]``.
+        """
 
         if isinstance(metric, str):
             metric = MV.convert_metric(metric)
@@ -40,7 +63,14 @@ class MV(Mv):
             kwargs['f'] = fct  # only forward this argument if we received it
         Mv.__init__(self, base, mvtype, ga=MV.GA, **kwargs)
 
-    def Fmt(self, fmt=1, title=None):
+    def Fmt(self, fmt=1, title=None) -> None:
+        """
+        ``Fmt`` in ``MV`` has inputs identical to ``Fmt`` in ``Mv`` except that
+        if ``A`` is a multivector then ``A.Fmt(2,'A')`` executes a print
+        statement from ``MV`` and returns ``None``, while from ``Mv``,
+        ``A.Fmt(2,'A')`` returns a string so that the function is compatible
+        with use in *ipython notebook*.
+        """
         print(Mv.Fmt(self, fmt=fmt, title=title))
 
 
