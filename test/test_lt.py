3e1b55d60b1d5e4257fc4218f31ee80ffd16ee52
Eric Wieser <wieser.eric@gmail.com>
2020-05-13 14:32:50 +0100

    Add tests for creating Mlt instances from a string

--- test/test_lt.py
+++ test/test_lt.py
@@ -60,3 +60,36 @@ class TestMlt(unittest.TestCase):
         assert T(a1, alpha * a2) == alpha * T(a1, a2)
         assert T(a1 + b, a2) == T(a1, a2) + T(b, a2)
         assert T(a1, a2 + b) == T(a1, a2) + T(a1, b)
+
+    def test_from_str(self):
+        coords = symbols('x y', real=True)
+        g, e1, e2 = Ga.build('e*1|2', coords=coords, g=[1, 1])
+
+        a1 = g.mv('a1', 'vector')
+        a2 = g.mv('a2', 'vector')
+        a1x, a1y = a1.get_coefs(1)
+        a2x, a2y = a2.get_coefs(1)
+
+        # one-d
+        T = Mlt('T', g, nargs=1)
+        v = T(a1)
+
+        # Two new symbols created
+        Tx, Ty = sorted(v.free_symbols - {a1x, a1y}, key=lambda x: x.sort_key())
+        assert v == (
+            Tx * a1x +
+            Ty * a1y
+        )
+
+        # two-d
+        T = Mlt('T', g, nargs=2)
+        v = T(a1, a2)
+
+        # four new symbols created
+        Txx, Txy, Tyx, Tyy = sorted(v.free_symbols - {a1x, a1y, a2x, a2y}, key=lambda x: x.sort_key())
+        assert v == (
+            Txx * a1x * a2x +
+            Txy * a1x * a2y +
+            Tyx * a1y * a2x +
+            Tyy * a1y * a2y
+        )
