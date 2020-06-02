51fd5339dd2af52561954a48dce68d3e7d1140f3
Eric Wieser <wieser.eric@gmail.com>
2020-06-02 09:06:25 +0100

    Add tests for lazy_dict

--- test/test_misc.py
+++ test/test_misc.py
@@ -1,8 +1,12 @@
+import re
+import string
 import sys
+import textwrap
 
 import pytest
+from IPython.lib.pretty import pretty
 
-from galgebra.ga import OrderedBiMap
+from galgebra.ga import lazy_dict, OrderedBiMap
 from galgebra._utils import cached_property
 
 
@@ -54,3 +58,32 @@ class TestCachedProperty:
                 pass
 
         assert X.__annotations__['val'] == 'int'
+
+
+class TestLazyDict:
+
+    def test_caching(self):
+        seen = []
+        def f_value(k):
+            seen.append(k)
+            return len(seen)
+
+        l = lazy_dict({'a': 0}, f_value)
+        assert l['a'] == 0
+        assert len(seen) == 0
+
+        # should not call the function twice
+        assert l['b'] == 1
+        assert l['b'] == 1
+
+        assert l['c'] == 2
+
+    def test_repr(self):
+        l = lazy_dict({'1': 1}, int)
+        assert repr(l) == "lazy_dict({'1': 1}, f_value=<class 'int'>)"
+
+        # needs to be long enough to wrap
+        l = lazy_dict({string.ascii_lowercase: string.ascii_lowercase }, int)
+        assert pretty(l) == textwrap.dedent("""\
+            lazy_dict({'abcdefghijklmnopqrstuvwxyz': 'abcdefghijklmnopqrstuvwxyz'},
+                      f_value=<class 'int'>)""")
