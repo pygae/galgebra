b57b4fe7ed2ef3b87e131e89d0c0be9939b7bb8e
utensil <utensilcandel@gmail.com>
2024-03-30 16:40:28 +0800

    Tweaks and workarounds to  fix the doc style locally

--- doc/conf.py
+++ doc/conf.py
@@ -53,6 +53,7 @@ extensions = [
     'sphinx_markdown_tables',
     'releases',
     'sphinxcontrib.bibtex',
+    'sphinx_rtd_theme',
 
     # local extensions
     'md_include',
@@ -231,7 +232,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -268,7 +269,7 @@ releases_unstable_prehistory = True
 #
 html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -285,6 +286,7 @@ html_static_path = ['_static']
 
 html_context = {
     'css_files': [
+        '_static/css/theme.css',
         '_static/theme_overrides.css',  # override wide tables in RTD theme
         ],
      }
