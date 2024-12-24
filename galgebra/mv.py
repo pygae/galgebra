372df9c9a477e3d66c31bf2670f430ea537fd6d5
Jonas Spinner <jonasspinner@gmx.de>
2024-12-24 17:46:48 +0100

    Added hitzer_inverse following the clifford package implementation

--- galgebra/mv.py
+++ galgebra/mv.py
@@ -1377,6 +1377,53 @@ class Mv(printer.GaPrintable):
         detU = -Uk.scalar()
         return adjU / detU
 
+    def hitzer_inverse(self) -> 'Mv':
+        """
+        Efficient algorithm for the inverse in n<6 following
+        Eckhard Hitzer, Stephen Sangwine (2017)
+        'Multivector and multivector matrix inverses in real Clifford algebras'
+        """
+        n = self.Ga.n
+        if n == 0:
+            numerator = 1 + 0 * self
+        elif n == 1:
+            # Equation 4.3
+            numerator = self.g_invol()
+        elif n == 2:
+            # Equation 5.5
+            numerator = self.ccon()
+        elif n == 3:
+            # Equation 6.5 without the rearrangement from 6.4
+            mv_conj = self.ccon()
+            mv_mul_mv_conj = self * mv_conj
+            numerator = mv_conj * ~mv_mul_mv_conj
+        elif n == 4:
+            # Equation 7.7
+            mv_conj = self.ccon()
+            mv_mul_mv_conj = self * mv_conj
+            m34 = (
+                mv_mul_mv_conj
+                - 2 * mv_mul_mv_conj.get_grade(3)
+                - 2 * mv_mul_mv_conj.get_grade(4)
+            )
+            numerator = mv_conj * m34
+        elif n == 5:
+            # Equation 8.22 without the rearrangement from 8.21
+            mv_conj = self.ccon()
+            mv_mul_mv_conj = self * mv_conj
+            combo_op = mv_conj * ~mv_mul_mv_conj
+            mv_combo_op = self * combo_op
+            m14 = mv_combo_op - 2 * mv_combo_op.get_grade(1) - 2 * mv_combo_op.get_grade(4)
+            numerator = combo_op * m14
+        else:
+            raise NotImplementedError(
+                'Closed form inverses for algebras with more than 5 dimensions are not implemented'
+            )
+        denominator = (self * numerator).scalar()
+        if denominator == 0:
+            raise ValueError('Multivector has no inverse')
+        return numerator / denominator
+
     def func(self, fct) -> 'Mv':  # Apply function, fct, to each coefficient of multivector
         s = S.Zero
         for coef, base in metric.linear_expand_terms(self.obj):
